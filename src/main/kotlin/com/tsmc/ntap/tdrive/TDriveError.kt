7d0ae4bd9d2e571a69ae3a82cf599e4ae6156b90
hmchangm <dickgg@gmail.com>
2021-09-15 07:06:38 +0800

    add findbyID

--- src/main/kotlin/com/tsmc/ntap/tdrive/TDriveError.kt
+++ src/main/kotlin/com/tsmc/ntap/tdrive/TDriveError.kt
@@ -5,12 +5,14 @@ import javax.ws.rs.core.Response
 sealed class TDriveError {
     data class DatabaseProblem(val e :Throwable):TDriveError()
     data class FileReadError(val e :Throwable):TDriveError()
+    data class SomeError(val uuid: String):TDriveError()
 
     companion object {
         fun toResponse(e: TDriveError): Response = when (e) {
             is FileReadError -> Response.serverError().entity(e.e.stackTraceToString()).build()
-            is TDriveError.DatabaseProblem -> Response.serverError().entity("DbError ${e.e.stackTraceToString()}")
+            is DatabaseProblem -> Response.serverError().entity("DbError ${e.e.stackTraceToString()}")
                 .build()
+            is SomeError -> Response.serverError().entity("SomeError").build()
         }
     }
 }
--- src/main/kotlin/com/tsmc/ntap/tdrive/repo/FruitRepo.kt
+++ src/main/kotlin/com/tsmc/ntap/tdrive/repo/FruitRepo.kt
@@ -1,6 +1,6 @@
 package com.tsmc.ntap.tdrive.repo
 
-import arrow.core.Either
+import arrow.core.*
 import com.tsmc.ntap.tdrive.TDriveError
 import com.tsmc.ntap.tdrive.bean.Fruit
 import com.vladsch.kotlin.jdbc.Row
@@ -28,8 +28,21 @@ class FruitRepo {
                     }
                 }.mapLeft { TDriveError.DatabaseProblem(it) }
             }
-        private const val insertSql: String = "insert into fruit (uuid,name,description) values (?, ?, ?)"
 
+        private const val findByIdSql = "select * from fruit where uuid = ?"
+        val findById: suspend (uuid: String) -> Either<TDriveError, Fruit> = {uuid->
+            runCatching {usingDefault { session ->
+                session.first(sqlQuery(findByIdSql,uuid), toFruit)
+            }}.fold(
+            { fruit->
+                 when (val one = fruit.toOption()) {
+                    is Some -> one.value.right()
+                    is None -> TDriveError.SomeError(uuid).left()
+                }
+            }, {  Either.Left(TDriveError.DatabaseProblem(it)) })
+        }
+
+        private const val insertSql: String = "insert into fruit (uuid,name,description) values (?, ?, ?)"
         val add: suspend (fruit: Fruit) -> Either<TDriveError, Fruit> = {fruit->
             Either.catch {
                 usingDefault { session ->
@@ -39,8 +52,20 @@ class FruitRepo {
                     }
                     one
                 }
-
             }.mapLeft { TDriveError.DatabaseProblem(it) }
         }
+
+        private const val updateSql: String = "update fruit set name=?,description=? where uuid=?"
+        val update: suspend (fruit: Fruit) -> Either<TDriveError,Unit> = {one->
+            Either.catch {
+                usingDefault { session ->
+                    session.transaction { tx ->
+                        tx.update(sqlQuery(updateSql,  one.name, one.description,one.uuid))
+                    }
+                }
+                Unit
+            }.mapLeft { TDriveError.DatabaseProblem(it) }
+        }
+
     }
 }
